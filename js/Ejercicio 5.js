bc91e21f4605bfd2781885a5f2d0cf56d42df12b
Roxana Coronel <ricoronel@misena.edu.co>
2018-11-09 13:45:19 -0500

    ultimo

---
+++ js/Ejercicio 5.js
@@ -0,0 +1,18 @@
+var cc=prompt("Ingrse su número de cédula");
+if(isNaN (cc)){
+    alert("Valor ingresado incorrecto")
+}
+
+
+var name=prompt("Ingrese su nombre");
+
+
+
+
+
+
+
+
+
+document.write("<h1>Cédula Empleado: "+ cc);
+document.write("<h1>Nombre Empleado: "+ name);
