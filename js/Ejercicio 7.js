f317292b95a7c4f90cb2eb8e65f93f7a29b1aba8
2209angelgomez <41799942+2209angelgomez@users.noreply.github.com>
2018-11-30 17:02:53 -0500

    ju

--- js/Ejercicio 7.js
+++ js/Ejercicio 7.js
@@ -4,40 +4,46 @@ var agewomen=0;
 var agemen=0;
 var gender;
 var age;
-var sum;
-var avgwomen;
-var avgmen;
+var total;
+var averageWomen;
+var averageMen;
 
 
 gender=prompt("Por favor ingrese su género");
 age=parseInt(prompt("Por favor ingrese su edad"));
 
-while(gender!=isNaN && isNaN(age)){
-    alert("Por favor ingrese valores validos");
-    gender=prompt("Por favor ingrese su genero");
-    age=parseInt(prompt("Por favor ingrese su edad"));
-}
+
 while (age<18){
     alert("No se admiten menores de edad")
     gender=prompt("Por favor ingrese su genero");
     age=parseInt(prompt("Por favor ingrese su edad"));
 }
 
+while(gender!=isNaN && isNaN(age)){
+    alert("Por favor ingrese valores válidos");
+    gender=prompt("Por favor ingrese su genero");
+    age=parseInt(prompt("Por favor ingrese su edad"));
+}
+
 while (age!=0) {
-    if(gender=="hombre"){
-        countmen++;
-        agemen=agemen+age;
-    }
-    else if(gender=="mujer"){
+    if(gender=="Mujer"){
         countwomen++;
-        agewomen=agewomen+age; 
+        agewomen=agewomen+age;
+    }
+    else if(gender=="Hombre"){
+        countmen++;
+        agemen=agemen+age; 
     }
     gender=prompt("Por favor ingrese su genero");
     age=parseInt(prompt("Por favor ingrese su edad"));
 }
 
-sum=countmen+countwomen;
-avgwomen=agewomen/countwomen;
-avgmen=agemen/countmen;
-document.write("<p>A la fiesta ingresaron "+sum+"</p><p>Ingresaron "+countmen+" hombres</p><p>Ingresaron "+countwomen+" mujeres");
-document.write("<p>El promedio de edad por hombres es de: "+avgmen+"</p><p>El promedio de edad por mujeres es de: "+avgwomen+"</p>");
+total=countmen+countwomen;
+averageWomen=agewomen/countwomen;
+averageMen=agemen/countmen;
+
+document.write("<h1> La cantidad total de personas que ingresó a la fiesta fue de: "+total+"</h1>");
+document.write("<h1> La cantidad de hombres que ingresó a la fiesta fue de: " +countmen+ "</h1>");
+document.write("<h1> La cantidad de mujeres que ingresó a la fiesta fue de: " +countwomen+"</h1>" );
+document.write("<h1> El promedio de edad de los hombres que ingresaron es de: "+averageMen+ "</h1>");
+document.write("<h1> El promedio de edad de las mujeres que ingresaron es de: "+averageWomen+"</h1>");
